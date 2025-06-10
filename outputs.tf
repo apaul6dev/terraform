c332ea0ec4cb7bd2c8cbdcd2c8106e52d5965cb5
Paul Gallegos Aguilar <apaul.gallegos@gmail.com>
2025-06-10 06:00:46 -0500

    change architecture

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,16 @@
-output "ec2_public_ip" {
-  description = "La IP pública de la instancia EC2"
-  value       = aws_instance.unir_tests.public_ip
+# Outputs
+output "frontend_public_ips" {
+  value = [for instance in aws_instance.frontend : instance.public_ip]
 }
 
-output "ec2_private_ip" {
-  description = "La IP privada de la instancia EC2"
-  value       = aws_instance.unir_tests.private_ip
+output "backend_private_ips" {
+  value = [for instance in aws_instance.backend : instance.private_ip]
+}
+
+output "frontend_alb_dns" {
+  value = aws_lb.frontend_alb.dns_name
+}
+
+output "backend_alb_dns" {
+  value = aws_lb.backend_alb.dns_name
 }
