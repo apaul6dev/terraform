f8f74931b55fb0ccee2ae03727a53cc677e6fea0
Paul Gallegos Aguilar <apaul.gallegos@gmail.com>
2025-06-10 05:14:58 -0500

    set old

---
+++ main.old.tf
@@ -0,0 +1,124 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = var.secret_region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+locals {
+  extra_tag = "extra-tag"
+}
+
+# Data source to get latest Packer-generated AMI
+data "aws_ami" "packer" {
+  most_recent = true
+  owners      = ["self"]
+
+  filter {
+    name   = "name"
+    values = ["ami-nodenginx*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "unir-vpc"
+  }
+}
+
+# Subnet pública
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "unir-subnet"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "unir-igw"
+  }
+}
+
+# Tabla de rutas
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "unir-rt"
+  }
+}
+
+# Asociación de tabla de rutas a la Subnet
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.rt.id
+}
+
+# Security Group
+resource "aws_security_group" "ec2_sg" {
+  name        = "unir-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "unir-ec2-sg"
+  }
+}
+
+# Instancia EC2 usando la AMI personalizada de Packer
+resource "aws_instance" "unir_tests" {
+  ami                    = data.aws_ami.packer.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  tags = {
+    Name     = "unir-instance"
+    ExtraTag = local.extra_tag
+  }
+}
